843f85f3fc3ac6d5e2c5cf11ad8a283a47f3a890
Frappez Sylvain <sylvain.frappez@gmail.com>
2019-11-26 12:24:43 +0100

    {Add}Project

---
+++ Assets/Scripts/Tank/GameProtocol.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameProtocol 
+{
+
+    public enum Handler
+    {
+        POSITION = OnlineProtocol.Handler.GAME_PROTOCOL_START,
+    }
+}
